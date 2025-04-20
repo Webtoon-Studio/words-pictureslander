61fe72da87c601fd5e8f46a4bacf2034fb0202ef
Nen <sochimadn@gmail.com>
2025-04-20 14:58:18 +0100

    fixing images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: isProd ? '/words-pictureslander' : '',
+  assetPrefix: isProd ? '/words-pictureslander/' : '',
+  images: {
+    unoptimized: true, // required for static export
 };
 
 export default nextConfig;
