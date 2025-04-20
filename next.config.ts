6e2227b5b7c6cfd8e6b8d290a5fbf4cc953e1ad6
Nen <sochimadn@gmail.com>
2025-04-20 15:44:55 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,12 @@ import type { NextConfig } from 'next';
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   basePath: isProd ? '/words-pictureslander' : '',
   assetPrefix: isProd ? '/words-pictureslander/' : '',
   images: {
-    unoptimized: true, // required for static export
-  }.
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
